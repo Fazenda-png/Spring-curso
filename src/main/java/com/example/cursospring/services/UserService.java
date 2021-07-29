0f087214779bebb9bba78fcebc1d88569b232299
Fazenda-jpg <fazenda.j.p.s@hotmail.com>
2021-07-29 20:07:44 -0300

    Exception findById

--- src/main/java/com/example/cursospring/services/UserService.java
+++ src/main/java/com/example/cursospring/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.example.cursospring.entities.User;
 import com.example.cursospring.repositories.UserRepository;
+import com.example.cursospring.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 
 	public User findById(Long Id) {
 		Optional<User> obj = userRepository.findById(Id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(Id));
 	}
 
 	public User insert(User obj) {

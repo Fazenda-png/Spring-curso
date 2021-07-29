3385166da1ee28a531e278c378502bb6cf87efcf
Fazenda-jpg <fazenda.j.p.s@hotmail.com>
2021-07-28 21:46:19 -0300

    Product

---
+++ src/main/java/com/example/cursospring/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.example.cursospring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.cursospring.entities.Product;
+import com.example.cursospring.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository productRepository;
+
+	public List<Product> findAll() {
+		return productRepository.findAll();
+	}
+
+	public Product findById(Long Id) {
+		Optional<Product> obj = productRepository.findById(Id);
+		return obj.get();
+	}
+}
